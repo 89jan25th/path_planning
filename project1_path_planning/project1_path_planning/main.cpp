e8c348af7e1e30b66bbc803ac87f1ce5e5e073c9
Jiho Choi <catcher@Jihos-MacBook-Pro.local>
2018-02-04 18:59:39 +0900

    first commit

---
+++ project1_path_planning/project1_path_planning/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  project1_path_planning
+//
+//  Created by Jiho Choi on 03/02/2018.
+//  Copyright © 2018 Jiho Choi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
